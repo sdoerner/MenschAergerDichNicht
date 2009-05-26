0ede99895b67e1d00327777262fe1a5598c04d47
rainer <rainerkaergel@web.de>
2009-05-26 16:09:45 +0200

    Figur, Spieler, Spielfeld hinzu, MenschMain geaendert

--- src/aufgabe6/Figur.java
+++ src/aufgabe6/Figur.java
@@ -1,6 +1,70 @@
 package aufgabe6;
 
+/**
+ * Figur repraesentiert eine Spielfigur inklusive ihrem Besitzer (Spieler) und ihrer Position auf dem Spielfeld
+ * @author rainer
+ *
+ */
 public class Figur {
-	private Spieler owner;
+	private Spieler besitzer;
 	private int position;
+	
+	/**
+	 * Konstruktor; setzt den Besitzer und die Position auf -1 (noch nicht im Spiel)
+	 * @param besitzer der Besitzer der Figur
+	 */
+	public Figur(Spieler besitzer) {
+		this.besitzer = besitzer;
+		this.position = -1;
+	}
+	
+	/**
+	 * bewegt die Figur um eine bestimmte Anzahl an Schritten
+	 * @param anzahlSchritte die Anzahl der Schritte, um die die Figur bewegt werden soll
+	 */
+	public void bewege(int anzahlSchritte) {
+		this.position += anzahlSchritte;
+		
+		if (!istInZiel() && !istNaheZiel())
+			this.position %= 40;
+	}
+	
+	/**
+	 * gibt die aktuelle Position der Figur auf dem Spielbrett zurueck
+	 * @return die aktuelle Position
+	 */
+	public int getPosition() {
+		return position;
+	}
+
+	/**
+	 * gibt den Besitzer der Figur zurueck
+	 * @return
+	 */
+	public Spieler getBesitzer() {
+		return besitzer;
+	}
+	
+	/**
+	 * entfernt diese Figur aus dem Spiel
+	 */
+	public void entferne() {
+		this.position = -1;
+	}
+	
+	/**
+	 * gibt an, ob die Figur nahe an ihrem Ziel, aber noch auf dem Spielbrett ist
+	 * @return wahr, wenn die Figur in 6 Schritten das Ziel erreichen kann, sonst falsch
+	 */
+	public boolean istNaheZiel() {
+		return (this.position + 39) % 40 > ((besitzer.getEinstiegspunkt() - 6) + 39) % 40;
+	}
+	
+	/**
+	 * gibt an, ob die Figur schon im Ziel ist
+	 * @return
+	 */
+	public boolean istInZiel() {
+		return this.position > 39;
+	}
 }
--- src/aufgabe6/MenschMain.java
+++ src/aufgabe6/MenschMain.java
@@ -1,15 +1,62 @@
 package aufgabe6;
 
-public class MenschMain
-{
+import java.util.Vector;
 
+public class MenschMain {
+	private static Vector<Spieler> spieler = null;
+	private static Spieler aktuellerSpieler = null;
+
+	/**
+	 * wuerfelt und laesst den aktuellen Spieler ziehen (bei gewuerfelter 6 Wiederholung)
+	 */
+	private static void wuerfelnUndZiehen() {
+		int gewuerfelteZahl;
+		
+		do {
+			gewuerfelteZahl = Spielfeld.getInstance().wuerfeln();
+			aktuellerSpieler.ziehe(gewuerfelteZahl);
+		} while (gewuerfelteZahl == 6);
+	}
+	
     /**
+     * initialisiert und startet das Spiel
      * @param args
      */
-    public static void main(String[] args)
-    {
-        // TODO Auto-generated method stub
-        System.out.println("test");;
+    public static void main(String[] args) {
+        spieler = new Vector<Spieler>(4);
+        verbindeSpieler("Sascha");
+        
+        boolean istSiegerGefunden = false;
+        
+        while (!istSiegerGefunden) {
+        	for (Spieler itSpieler : spieler) {
+        		aktuellerSpieler = itSpieler;
+        		
+        		if (!itSpieler.istDraussen()) {
+        			for (int i = 0; i < 3; i++) {
+        				int gewuerfelteZahl = Spielfeld.getInstance().wuerfeln();
+        				
+        				if (gewuerfelteZahl == 6) {
+        					itSpieler.ziehe(6);
+        					wuerfelnUndZiehen();
+        					break;
+        				}
+        			}
+        		} else
+        			wuerfelnUndZiehen();
+        		
+	        	if (itSpieler.hatGewonnen()) {
+	        		istSiegerGefunden = true;
+	        		break;
+	        	}
+        	}
+        }
     }
 
+    /**
+     * fuege einen neuen Spieler zum Spiel hinzu
+     */
+    public static void verbindeSpieler(String name) {
+    	spieler.add(new Spieler(name, spieler.size()));
+    }
 }
