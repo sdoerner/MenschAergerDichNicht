f7961b0270091978117727c055758b366ce7f4e6
Sebastian Dörner <sdoerner@st.ovgu.de>
2009-05-26 10:48:41 +0200

    Initial Commit

---
+++ src/aufgabe6/MenschMain.java
@@ -0,0 +1,15 @@
+package aufgabe6;
+
+public class MenschMain
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        System.out.println("test");;
+    }
+
+}
